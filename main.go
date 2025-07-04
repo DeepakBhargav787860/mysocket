5ba066596133897a659c76610ed737ec96a12cc8
DeepakBhargav787860 <Deepak.Bhargav787860@gmail.com>
2025-07-04 13:22:50 +0530

    chnages

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"bytes"
 	businesslogic "chatapp/businessLogic"
 	"chatapp/db"
 	"chatapp/global"
 	"chatapp/route"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
+
+type LogtailWriter struct {
+	endpoint string
+}
+
+func (w *LogtailWriter) Write(p []byte) (n int, err error) {
+	logMsg := []byte(`{"message": "` + string(bytes.TrimSpace(p)) + `"}`)
+	resp, err := http.Post(w.endpoint, "application/json", bytes.NewBuffer(logMsg))
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	return len(p), nil
+}
 
 func main() {
 	// Database connect
@@ -18,7 +35,11 @@ func main() {
 	} else {
 		log.Println("database connected successfully 😘😘😘😘😘😘😘")
 	}
+	//logs
+	logtailEndpoint := "https://s1369505.eu-nbg-2-vec.betterstackdata.com"
 
+	// Logtail + Terminal dono me logs bhejna
+	log.SetOutput(io.MultiWriter(os.Stdout, &LogtailWriter{endpoint: logtailEndpoint}))
 	// Routes
 	route.Routes()
 	go businesslogic.HandleMessages()
