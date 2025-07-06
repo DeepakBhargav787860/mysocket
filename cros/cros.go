ebf1293fa40efb298f16aea239f247cc712853ec
DeepakBhargav787860 <Deepak.Bhargav787860@gmail.com>
2025-07-06 13:54:25 +0530

    chnages

--- cros/cros.go
+++ cros/cros.go
@@ -9,12 +9,13 @@ func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 		log.Println("origin", origin)
-		if origin == "https://chat-sj5k-deepaks-projects-a5241927.vercel.app" || origin == "http://localhost:5173" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,token")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
-		}
+		// if origin == "https://chat-sj5k-deepaks-projects-a5241927.vercel.app" || origin == "http://localhost:5173" {
+
+		// }
+		w.Header().Set("Access-Control-Allow-Origin", "https://chat-sj5k-deepaks-projects-a5241927.vercel.app")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,token")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
